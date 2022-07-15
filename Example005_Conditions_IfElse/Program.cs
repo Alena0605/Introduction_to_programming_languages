998d7affe48bbf1c88f792628fdbc7270886435d
Alena <ABelova0605@gmail.com>
2022-07-15 23:36:35 +0300

    add folder Example005_Conditions_IfElse

---
+++ Example005_Conditions_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "алена")
+{
+    Console.WriteLine("Ура, это же АЛЕНА! :)");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
