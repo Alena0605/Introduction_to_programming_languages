dc57da0ee05ad59b48f0fcc5e9c72a5e5499a724
Alena <ABelova0605@gmail.com>
2022-07-21 15:26:28 +0300

    change folder Example009

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -1,4 +1,13 @@
-﻿int[] array = {11, 211, 31, 41, 15, 61, 17, 18, 19};
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+
+int[] array = {11, 211, 31, 41, 15, 61, 17, 18, 19};
 
 int max = Max(
     Max(array[0], array[1], array[2]),
